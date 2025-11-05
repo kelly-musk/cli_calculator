f84d91c041845169f070d8adf8fd7551b474bbdc
kelly-musk <ogunjioforkelechi@gmail.com>
2025-11-05 10:51:21 +0100

    chore:  update for repl implementation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
+// use ariadne::Line;
+// use chumsky::error;
 use clap::{Parser, Subcommand};
-use std::process;
+use std::{io::{self, BufRead, Write}, process};
 
 // calculator info
 #[derive(Debug, Parser)]
 #[command(name = "calculator")]
 #[command(about = "A fast and simple calculator ")]
-struct CliCalculator {
+pub struct CliCalculator {
     #[command(subcommand)]
-    operations: Commands,
+    operations: Option<Commands>,
 }
 
 #[derive(Subcommand, Debug)]
-enum Commands {
+pub enum Commands {
+    Repl,
     /// addition 
     #[command(name = "add")]
     Add { a: f64, b: f64 },
@@ -25,11 +28,16 @@ enum Commands {
     #[command(name = "div")]
     Div { a: f64, b: f64 },
 }
+pub fn main() {
+    let calculator = CliCalculator::parse();
 
-fn main() {
-    let clicalculator = CliCalculator::parse();
+    let operations = calculator.operations.unwrap_or_else(|| {
+        CliCalculator::parse_from(["Clac", "--help"]);
+        std::process::exit(0)
+    });
 
-    match clicalculator.operations {
+    match operations {
+        Commands::Repl => run_repl(),
         Commands::Add { a, b } => println!("{}", a + b),
         Commands::Sub { a, b } => println!("{}", a - b),
         Commands::Mul{ a, b } => println!("{}", a * b),
@@ -40,6 +48,74 @@ fn main() {
             } else {
                 println!("{}", a / b)
             }
+        }
+    }
+}
+
+
+
+fn run_repl(){
+    println!("Welcom my calculator app built with repl crate");
+    println!("Name of author is kelly Musk");
+    println!("commands guide through this app are exit, add, sub, div");
+
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+
+    loop {
+        print!("> ");
+        io::stdout().flush().expect("failed to flush stdout");
+
+        let input = match lines.next() {
+            Some(Ok(line)) => line.trim().to_string(),
+            Some(Err(e)) => {
+                eprintln!("Input error {}", e);
+                continue;
+            }
+            None => break,
+        };
+        if input.is_empty(){
+            continue;
+        }
+        if matches!(input.as_str(), "exit" | "quit"){
+            println!("Goodbye!");
+            break;
+        }
+    
+        let parts: Vec<&str> = input.split_whitespace().collect();
+        if parts.len() != 3 {
+            eprintln!("usage; <cmd> <num1> <num3>");
+            continue;
+        }
+        let cmd = parts[0];
+        let a = match parts[1].parse::<f64>() {
+            Ok(n) => n,
+            Err(_) => {
+                eprintln!("Invalid number {}", parts[1]);
+                continue;
+            }
+        };
+
+        let b = match parts[2].parse::<f64>() {
+            Ok(n) => n,
+            Err(_) => {
+                eprintln!("Invalid number {}", parts[2]);
+                continue;
+            }
+        };
+        match cmd {
+            "add" => println!("{}", a + b),
+            "sub" => println!("{}", a - b),
+            "mul" => println!("{}", a * b),
+            "div" => {
+                if b == 0.0 {
+                    println!("Error: division by zero!")
+                } else {
+                    println!("{}", a / b)
+                }
+            }
+            _=> eprintln!("UNKNOWN COMMAND USE: {}. add/sub/mul/div", cmd)
         }
     }
 }
